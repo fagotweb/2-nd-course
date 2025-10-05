2a8aa531916356e229fcad2da39e46b0ba4ef73a
fagotweb <aleksandr.19.88@mail.ru>
2025-10-05 09:58:49 +0300

    Исправил и добавил table.scss

--- hw-2.js
+++ hw-2.js
@@ -1,18 +1,18 @@
-// задача 1
+// задача #1
 let a = 10;
 alert(a);
 a = 20;
 alert(a);
 
-// задача 2
+// задача #2
 const year = 1861;
 alert(year + "год");
 
-// задача 3
+// задача #3
 let n = "Яша Скрипкин";
 alert(n);
 
-// задача 4
+// задача #4
 let number1 = 10;
 let number2 = 2;
 alert(String(number1 + number2));
@@ -20,16 +20,16 @@ alert(String(number1 - number2));
 alert(String(number1 * number2));
 alert(String(number1 / number2));
 
-// задача 5
+// задача #5
 let result = 2**5;
 alert(result);
 
-// задача 6
+// задача #6
 a = 9;
 let b = 2;
 alert(a % b);
 
-// задача 7
+// задача #7
 let num = 1;
 num += 5;
 num -= 3;
@@ -39,17 +39,17 @@ num ++;
 num --;
 alert(num);
 
-// задача 8
+// задача #8
 let age = prompt("Сколько вам лет?"); // Запрашиваем возраст
 alert(age); // Выводим возраст
 
-// задача 9
+// задача #9
 let user = {
   name: "Яша Скрипкин",
   age: 1861,
   isAdmin: false
 };
 
-// задача 10
+// задача #10
 let uName = prompt("Ваше имя?");
 alert(`Привет, ${uName}!`);
