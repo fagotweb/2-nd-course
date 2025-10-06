58398142a8ffa666079bcb6f7efa3a50f350559e
fagotweb <aleksandr.19.88@mail.ru>
2025-10-06 09:42:41 +0300

    Исправление домашки 3

--- hw-3.js
+++ hw-3.js
@@ -5,21 +5,21 @@ let p = prompt('Введите пароль');
 alert(p === password ? 'Пароль введен верно' : 'Пароль введен неправильно')
 
 // задача #2
-let c = '0';
+let c = 0;
 console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно')
 
-c = '10';
+c = 10;
 console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно')
 
-c = '-3';
+c = -3;
 console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно')
 
-c = '2';
+c = 2;
 console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно')
 
 // задача #3
-let d = '0';
-let e = '100';
+let d = 0;
+let e = 100;
 
 console.log(d > 100 || e > 100 ? 'Верно' : 'Неверно')
 
@@ -35,16 +35,16 @@ if (monthNumber > 13) {
     alert('Ошибка');
 } else {
     switch (monthNumber) {
-        case 1 || 2 || 12:
+        case 1: case 2: case 12:
             alert('Зима');
             break;
-        case 3 || 4 || 5:
+        case 3: case 4: case 5:
             alert('Весна');
             break;
-        case 6 || 7 || 8:
+        case 6: case 7: case 8:
             alert('Лето');
             break;
-        case 9 || 10 || 11:
+        case 9: case 10: case 11:
             alert('Осень');
             break;
         default:
