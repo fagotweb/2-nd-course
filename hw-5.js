2d7ab6f289d26af9e782c0b2d5bf03226dd2c79a
fagotweb <aleksandr.19.88@mail.ru>
2025-11-09 21:02:31 +0300

    домашка 5

--- hw-5.js
+++ hw-5.js
@@ -1,18 +1,22 @@
 // задача #1 Возврат меньшего числа
+
 let minNum = (a, b) => Math.min(a, b);
 console.log (minNum (4, 8));
 
 // задача #2 Принимает и возвращает число
+
 let num = prompt ('Ввести число'); 
 if (num % 2) { console.log('Число нечётное!'); } 
 else { console.log('Число чётное!'); }
 
 
 // задача #3 Квадрат числа
+
 let squareNum = (c) => c**2;
 console.log (squareNum (8));
 
 // задача #4 Сколько лет
+
 function ageNum () {
    const userAge = parseInt(prompt ('Сколько лет'));
    if (userAge < 0) {
@@ -25,6 +29,7 @@ function ageNum () {
 ageNum ();
 
 // задача #5 Алгоритм
+
 function nonNum () {
    const oneNum = prompt ('Введите первое число');
    const twoNum = prompt ('Введите второе число');
@@ -38,6 +43,7 @@ function nonNum () {
 nonNum ();
 
 // задача #6 Число в кубе
+
 function cubeNum () {
    num = prompt ('Введите число');   
    let cube = num**3;
@@ -55,6 +61,7 @@ function cubeNum () {
 cubeNum ();
 
 // задача #7 Объекты и методы
+
 function getArea() { return Math.PI * this.radius * this.radius; }
 const circle1 = {radius: 3};
 circle1.getArea = getArea;
